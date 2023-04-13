0651c33bc18fd8699ebc9a487b7028bdf57b1a15
SeciLKrm <secilkurum10@gmail.com>
2023-04-14 02:18:35 +0300

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import BannerImage from "../assets/banner.jpg"
+
+const Home = () => {
+  return (
+    <div className='home' style={{backgroundImage: `url(${BannerImage})`}}>
+        <div className='headerContainer'>
+            <h1>Burger</h1>
+            <p>Amazing Tastes</p>
+            <Link to="/menu">
+                <button>Order Now</button>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Home
